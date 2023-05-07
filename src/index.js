57855fd96b909bab99952f8f161746b34f61e5c7
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-05-07 20:28:32 -0300

    feat: add .env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = require("./routes");
 const app = express();
+require("dotenv").config();
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.use("/", router);
 
