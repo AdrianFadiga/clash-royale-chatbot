52fe3f5d2cf47073e7168d87f4cf89a8dd96ab2c
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-05-08 01:14:45 -0300

    troca para camelCase

--- prisma/migrations/20230508041126_/migration.sql
+++ prisma/migrations/20230508041126_/migration.sql
@@ -1,24 +1,25 @@
 -- CreateTable
 CREATE TABLE "players" (
     "tag" TEXT NOT NULL,
-    "clan_tag" TEXT NOT NULL,
+    "clanTag" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "role" TEXT NOT NULL,
     "lastSeen" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
 CREATE TABLE "river_race_participants" (
     "tag" TEXT NOT NULL,
-    "clan_tag" TEXT NOT NULL,
+    "clanTag" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "fame" INTEGER NOT NULL,
     "decksUsed" INTEGER NOT NULL,
     "decksUsedToday" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL
+    "missingAttacks" INTEGER NOT NULL,
+    "isInClan" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateIndex
