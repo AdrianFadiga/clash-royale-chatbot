f28750ae05d6624ad6861ad95f306c2227d054e2
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-05-08 01:16:37 -0300

    feat: playerService

---
+++ src/services/PlayerService.js
@@ -0,0 +1,28 @@
+const { PlayerRepository } = require("../repositories");
+const { royaleApi } = require("../requests");
+
+const PlayerService = {
+	async getAllTags() {
+		const tags = await PlayerRepository.getAllTags();
+
+		return tags.map(({tag}) => tag);
+	},
+
+	async updateClanMembers() {
+		const clanInfo = await royaleApi.getClanInfo();		
+
+		const serializedClanMembers = serialize(clanInfo);
+
+		const updatedClanMembers = await PlayerRepository.updateClanMembers(serializedClanMembers);
+
+		return updatedClanMembers;
+	},
+};
+
+function serialize(clanInfo) {
+	return clanInfo.memberList.map(({tag, name, role, lastSeen}) => ({
+		tag, clanTag: clanInfo.tag, name, role, lastSeen, createdAt: new Date()
+	}));
+}
+
+module.exports = {PlayerService};
