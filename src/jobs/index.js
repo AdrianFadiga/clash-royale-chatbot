8f87cb44d7ef974153f50f9a4220f70f0b6030cc
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-05-19 14:34:55 -0300

    feat: serverLogJob

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,4 +1,5 @@
 const {updateClanMembers, extractRiverRaceParticipants} = require("./clashApi");
+const serverLogJob = require("./serverLogJob");
 
 
-module.exports = {extractRiverRaceParticipants, updateClanMembers};
+module.exports = {extractRiverRaceParticipants, updateClanMembers, serverLogJob};
